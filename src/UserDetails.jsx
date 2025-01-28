1cba3635b6cc8cd913e5b8453f4e5220f526869d
Vaibhav Agrawal <vaibhavagr711@gmail.com>
2025-01-28 12:53:48 +0530

    Login and user details forms styling

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -11,9 +11,11 @@ function UserDetails() {
   const { user, setUser, logout } = useUser();
   const [name, setName] = useState(user.name || "");
   const [email, setEmail] = useState(user.email || "");
+  const [isSaving, setIsSaving] = useState(false);
   const navigate = useNavigate();
 
   const handleSaveDetails = async () => {
+    setIsSaving(true);
     const updatedUser = { ...user, name, email };
 
     try {
@@ -28,6 +30,8 @@ function UserDetails() {
       navigate("/");
     } catch (error) {
       console.error("Error saving user details:", error);
+    } finally {
+      setIsSaving(false);
     }
   };
 
@@ -37,31 +41,88 @@ function UserDetails() {
   };
 
   return (
-    <div className="user-details-container">
-      <h2>User Details</h2>
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Mobile Number"
-        value={user.mobile}
-        readOnly
-      />
-      <input
-        type="email"
-        placeholder="Email ID"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <button onClick={handleSaveDetails}>Save Details</button>
-      <button onClick={handleLogout} className="logout-button">
-        Logout
-      </button>
-    </div>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center">User Details</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="space-y-6">
+            <div>
+              <label htmlFor="name" className="block  font-medium ">
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter Your Full Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  maxLength={25}
+                  className={`block w-full rounded-md bg-white px-3 py-1.5 text-base  outline-1 -outline-offset-1 outline-gray-300 placeholder:text-primary focus:outline-2 focus:-outline-offset-2 focus:outline-accent sm:`}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block  font-medium ">
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter Your Email ID"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className={`block w-full rounded-md bg-white px-3 py-1.5 text-base  outline-1 -outline-offset-1 outline-gray-300 placeholder:text-primary focus:outline-2 focus:-outline-offset-2 focus:outline-accent sm:`}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="mobile" className="block  font-medium ">
+                Mobile Number
+              </label>
+              <div className="mt-2">
+                <input
+                  id="mobile"
+                  name="mobile"
+                  type="text"
+                  placeholder="Enter WhatsApp Mobile Number"
+                  value={user.mobile}
+                  readOnly
+                  className={`block w-full rounded-md bg-white px-3 py-1.5 text-base  outline-1 -outline-offset-1 outline-gray-300 placeholder:text-primary focus:outline-2 focus:-outline-offset-2 focus:outline-accent sm:`}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                className="flex w-full justify-center rounded-md px-3 py-1.5  font-semibold shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
+                onClick={handleSaveDetails}
+                disabled={isSaving}
+              >
+                {isSaving ? "Saving..." : "Save Details"}
+              </button>
+            </div>
+
+            <div>
+              <button
+                className="secondary-btn flex w-full justify-center rounded-md px-3 py-1.5  font-semibold shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
